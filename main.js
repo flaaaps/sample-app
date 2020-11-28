a5df9c267e758b097d23b37688fe17f3fbd8cb77
flaaaps <julianwening4@gmail.com>
2020-11-28 11:20:36 +0100

    🐛 Only respond with progress percent

--- main.js
+++ main.js
@@ -1,69 +1,67 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 
 let mainWindow;
 
 function createWindow() {
-	mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
-		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false
-		}
-	});
-	mainWindow.loadFile('index.html');
-	mainWindow.on('closed', function () {
-		mainWindow = null;
-	});
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 
-	mainWindow.once('ready-to-show', () => {
-		autoUpdater.checkForUpdatesAndNotify();
-	});
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 }
 
 app.on('ready', () => {
-	createWindow();
+  createWindow();
 });
 
 app.on('window-all-closed', function () {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', function () {
-	if (mainWindow === null) {
-		createWindow();
-	}
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
-ipcMain.on('app_version', event => {
-	event.sender.send('app_version', { version: app.getVersion() });
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
 });
 
 autoUpdater.on('update-available', () => {
-	mainWindow.webContents.send('update_available');
+  mainWindow.webContents.send('update_available');
 });
 
 autoUpdater.on('update-downloaded', () => {
-	mainWindow.webContents.send('update_downloaded');
+  mainWindow.webContents.send('update_downloaded');
 });
 
-const log = require('electron-log');
 autoUpdater.logger = log;
-autoUpdater.on('download-progress', progressObj => {
-	let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
-	log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-	log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
-	sendStatusToWindow(log_message);
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = progressObj.percent + '%';
+  sendStatusToWindow(log_message);
 });
 
 function sendStatusToWindow(text) {
-	log.info(text);
-	mainWindow.webContents.send('update-progress', text);
+  log.info(text);
+  mainWindow.webContents.send('update-progress', text);
 }
 
 ipcMain.on('restart_app', () => {
-	autoUpdater.quitAndInstall();
+  autoUpdater.quitAndInstall();
 });
